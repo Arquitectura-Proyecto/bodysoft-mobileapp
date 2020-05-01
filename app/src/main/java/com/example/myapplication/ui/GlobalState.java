ba627abff8cc48ff2398843405a658295cbe8ad7
unknown <afsanchezsa@unal.edu.co>
2020-05-01 11:13:03 -0500

    change port

--- app/src/main/java/com/example/myapplication/ui/GlobalState.java
+++ app/src/main/java/com/example/myapplication/ui/GlobalState.java
@@ -5,7 +5,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class GlobalState extends ViewModel {
-    private static  String token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6MiwiUHJvZmlsZSI6ZmFsc2UsIlR5cGVJRCI6MSwiZXhwIjoxNTg4Mjk5NzgwfQ.mmTyIlOi0L99p8EMyfRpb9Zw2IBrtcrcczi7fuqd1Ho";
+    private static  String token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6MiwiUHJvZmlsZSI6ZmFsc2UsIlR5cGVJRCI6MSwiZXhwIjoxNTg4MzU2NDA1fQ.OkpgzwaDgtMI_0e-eCK3SlKzow1cGCbpZSUEVQaRglg";
     private MutableLiveData<String>  sharedString;
     public GlobalState(){
         this.sharedString=new MutableLiveData<>();
