f14be6d3f46c60522b5ea3b171ca5dff949b35d5
unknown <afsanchezsa@unal.edu.co>
2020-05-01 14:54:20 -0500

    added query getrequest by routine to graphql

--- app/src/main/java/com/example/myapplication/ui/Routine/Adapters/RequestAdapter.java
+++ app/src/main/java/com/example/myapplication/ui/Routine/Adapters/RequestAdapter.java
@@ -1,12 +1,29 @@
 package com.example.myapplication.ui.Routine.Adapters;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.apollographqlandroid.GetRequestByIdRoutineQuery;
+
+import java.util.List;
+
 public class RequestAdapter   extends RecyclerView.Adapter<RequestAdapter.ViewHolderRequest> {
+
+    List<GetRequestByIdRoutineQuery.Request> routineList;
+    NavController navigationController;
+    private Context context;
+
+    public RequestAdapter(List<GetRequestByIdRoutineQuery.Request> routineList, NavController navigationController, Context context) {
+        this.routineList = routineList;
+        this.navigationController = navigationController;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public RequestAdapter.ViewHolderRequest onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
